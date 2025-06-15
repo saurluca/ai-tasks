c15545855a5d438beb5a19f208d4919c16741da9
saurluca <spuckteller@gmail.com>
2025-06-15 13:39:44 +0200

    feat: simplify views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="bg-red-500">
+    <h1 class="text-3xl font-bold underline">Hello World</h1>
+  </div>
 </template>
