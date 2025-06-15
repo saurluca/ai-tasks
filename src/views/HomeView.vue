a8ef4edf74e594d6ba1f0bc61571fe64be1d8a64
saurluca <spuckteller@gmail.com>
2025-06-15 13:48:07 +0200

    feat: add hello world on first page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  <div class="bg-red-500">
-    <h1 class="text-3xl font-bold underline">Hello World</h1>
+  <div class="h-screen w-screen bg-green-200">
+    <h1 class="text-3xl font-bold underline">
+      Hello World
+    </h1>
   </div>
 </template>
